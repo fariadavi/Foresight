6495c066563b514666fbf6747d8194d1974981fa
Davi Faria <davi.faria@cortex.local>
2016-06-03 17:10:44 -0300

    final(?) jump fix

--- src/br/unirio/pcs/foresight/domain/Player.java
+++ src/br/unirio/pcs/foresight/domain/Player.java
@@ -12,7 +12,7 @@ public class Player extends Sprite {
 	private static final int PLAYER_POSITION_X = 30;
 	private static final int PLAYER_POSITION_Y = 500;
 	private static final int PLAYER_RUN_SPEED = 250;
-	private static final int PLAYER_JUMP_SPEED = 150;
+	private static final int PLAYER_JUMP_SPEED = 300;
 	private Image[] playerSprite = new Image[10];
 	private Image[] playerGunSprite = new Image [2];
 	public boolean[] key_states = new boolean[256];
@@ -20,8 +20,6 @@ public class Player extends Sprite {
 	private boolean Projectile;
 	private int steps = 0;
 	private double jumpVelocity = 0.0; 
-	private double que = 0;
-	private double frametimeJump;
 	private boolean onGround;
 
 	public Player() {
@@ -29,12 +27,13 @@ public class Player extends Sprite {
 		playerSprite[0] = new ImageIcon("images/characters/yellow/alienYellow_stand_right.png").getImage();
 		playerSprite[1] = new ImageIcon("images/characters/yellow/alienYellow_walk_right0.png").getImage();
 		playerSprite[2] = new ImageIcon("images/characters/yellow/alienYellow_walk_right1.png").getImage();
-		playerSprite[3] = new ImageIcon("images/characters/yellow/alienYellow_stand_left.png").getImage();
-		playerSprite[4] = new ImageIcon("images/characters/yellow/alienYellow_walk_left0.png").getImage();
-		playerSprite[5] = new ImageIcon("images/characters/yellow/alienYellow_walk_left1.png").getImage();
-		playerSprite[6] = new ImageIcon("images/characters/yellow/alienYellow_jump_right.png").getImage();
-		playerSprite[7] = new ImageIcon("images/characters/yellow/alienYellow_jump_left.png").getImage();
-		playerSprite[8] = new ImageIcon("images/characters/yellow/alienYellow_hurt_right.png").getImage();
+		playerSprite[3] = new ImageIcon("images/characters/yellow/alienYellow_jump_right.png").getImage();
+		playerSprite[4] = new ImageIcon("images/characters/yellow/alienYellow_hurt_right.png").getImage();
+
+		playerSprite[5] = new ImageIcon("images/characters/yellow/alienYellow_stand_left.png").getImage();
+		playerSprite[6] = new ImageIcon("images/characters/yellow/alienYellow_walk_left0.png").getImage();
+		playerSprite[7] = new ImageIcon("images/characters/yellow/alienYellow_walk_left1.png").getImage();
+		playerSprite[8] = new ImageIcon("images/characters/yellow/alienYellow_jump_left.png").getImage();
 		playerSprite[9] = new ImageIcon("images/characters/yellow/alienYellow_hurt_left.png").getImage();
 //		playerGunSprite[0] = new ImageIcon("images/weapons/raygunPurple_right.png").getImage();
 		playerGunSprite[1] = new ImageIcon("images/weapons/raygunPurple_left.png").getImage();
@@ -47,34 +46,35 @@ public class Player extends Sprite {
 
 	@Override
 	public void update(double differenceTime) {
-		que = differenceTime;
 		Projectile = false;
-		
-		// Player movements controller
 		if ((key_states[KeyEvent.VK_RIGHT]) && (positionX < 956))
 			walk(differenceTime, 1, 1);
 		else if ((key_states[KeyEvent.VK_LEFT]) && (positionX > -4))
-			walk(differenceTime, 4, -1);
-		else if (steps > 0 && steps < 3)
+			walk(differenceTime, 6, -1);
+		else if(steps >= 0 && steps < 3)
 			steps = 0;
-		else if (steps > 3 && steps < 6)
-			steps = 3;
+		else if (steps >= 5 && steps < 8)
+			steps = 5;
 		
+		// Player movements controller
 		if (key_states[KeyEvent.VK_UP]) {
-			frametimeJump += differenceTime;
-			if(frametimeJump < 0.5) {
+			if(onGround) {
 				jumpVelocity = -1 * speedY * differenceTime;
 				onGround = false;
 			}
-			if(key_states[KeyEvent.VK_RIGHT] || (steps >= 0 && steps < 3))
+			if(key_states[KeyEvent.VK_RIGHT] || (steps >= 0 && steps < 5))
+				steps = 3;
+			else if(key_states[KeyEvent.VK_LEFT] || (steps >= 5 && steps < 10))
 				steps = 8;
-			else if(key_states[KeyEvent.VK_LEFT] || (steps >= 3 && steps < 6))
-				steps = 9;
 		}
 
 		if(onGround) {
+			if(steps == 3)
+				steps = 0;
+			else if(steps == 8)
+				steps = 5;
+			
 			jumpVelocity = 0;
-			frametimeJump = 0;
 		} else {
 			jumpVelocity += gravity * differenceTime;
 		}
