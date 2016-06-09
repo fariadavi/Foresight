092664e89f44825d156d02f170d2304221d563d1
Davi Faria <davi.faria@cortex.local>
2016-06-09 15:56:48 -0300

    commit recordes

---
+++ src/br/unirio/pcs/foresight/domain/ManipulaArquivo.java
@@ -0,0 +1,127 @@
+package br.unirio.pcs.foresight.domain;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ManipulaArquivo {
+	
+	private static final String FILE = "pontuacao.dat";
+	private static final int MAX_LINHAS_ARQUIVO = 10;
+
+	public static void adicionaPontuacao(String nomeEntrado, int pts) {
+		try {
+			BufferedWriter file = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(FILE, true)));
+			file.append(nomeEntrado + "|" + pts + "\n");
+			file.close();
+		} catch (FileNotFoundException ex) {
+			System.out.println("Erro: " + ex.getMessage());
+		} catch (IOException ex) {
+			System.out.println("Erro: " + ex.getMessage());
+		}
+		
+		organiza();
+	}
+
+	public static void escreve(String[] lines) {
+		try {
+			if (lines.length > MAX_LINHAS_ARQUIVO)
+				lines = Arrays.copyOfRange(lines, 0, MAX_LINHAS_ARQUIVO);
+
+			BufferedWriter file = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(FILE, false)));
+			for (String string : lines) {
+				file.append(string);
+				file.append("\n");
+			}
+			
+			file.close();
+		} catch (FileNotFoundException ex) {
+			System.out.println("Erro: " + ex.getMessage());
+		} catch (IOException ex) {
+			System.out.println("Erro: " + ex.getMessage());
+		}
+	}
+	
+	public static void organiza() {
+		List<String> lines = leitura();
+		int size = lines.size();
+		String[] linhas = lines.toArray(new String[size]);
+		
+		for (int i = 0; i < size; i++) {
+			int pontosA = Integer.parseInt(linhas[i].split("\\|")[1]);
+			
+			for(int j = i+1; j < size; j++) {
+				int pontosB = Integer.parseInt(linhas[j].split("\\|")[1]);
+				
+				if(pontosB > pontosA) {
+					String temp = linhas[j];
+					linhas[j] = linhas[i];
+					linhas[i] = temp;
+					
+					int temp2 = pontosB;
+					pontosB = pontosA;
+					pontosA = temp2;
+				}
+			}
+		}
+		escreve(linhas);
+	}
+	
+	public static String[] getNomes() {
+		List<String> leitura = leitura();
+		
+		String[] nomes = new String[leitura.size()];
+		int i = 0;
+		for (String linha : leitura) {
+			nomes[i] = linha.split("\\|")[0];
+			i++;
+		}
+		
+		return nomes;
+	}
+
+	public static int[] getPts() {
+		List<String> leitura = leitura();
+		
+		int[] pontos = new int[leitura.size()];
+		int i = 0;
+		for (String linha : leitura) {
+			pontos[i] = Integer.parseInt(linha.split("\\|")[1]);
+			i++;
+		}
+		
+		return pontos;
+	}
+
+	public static List<String> leitura() {
+		List<String> linhas = new ArrayList<String>();
+		
+		try {
+			BufferedReader fileread = new BufferedReader(new InputStreamReader(new FileInputStream(FILE)));
+			String linha;
+			while((linha = fileread.readLine()) != null)
+				linhas.add(linha);
+			
+			fileread.close();
+		} catch (FileNotFoundException ex) {
+			System.out.println("Erro: " + ex.getMessage());
+		} catch (IOException ex) {
+			System.out.println("Erro: " + ex.getMessage());
+		}
+		
+		return linhas;
+	}
+	
+	public static int menorPont() {
+		int[] pts = getPts();
+		return pts[pts.length-1];
+	}
+}
---
+++ src/br/unirio/pcs/foresight/domain/Recordes.java
@@ -0,0 +1,65 @@
+package br.unirio.pcs.foresight.domain;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics2D;
+import java.awt.GradientPaint;
+import java.awt.geom.Rectangle2D;
+import java.awt.geom.RoundRectangle2D;
+import java.io.IOException;
+
+public class Recordes {
+	private double px = 365, py = 610;
+	private boolean ativo = false;
+	private String[] nomes;
+	private int[] pts;
+
+	public Recordes() {
+	}
+
+	public boolean ativo() {
+		return ativo;
+	}
+
+	public void ativar() throws IOException {
+		ativo = true;
+		if (nomes == null || pts == null) {
+			nomes = ManipulaArquivo.getNomes();
+			pts = ManipulaArquivo.getPts();
+		}
+	}
+
+	public void desativar() {
+		ativo = false;
+		px = 365;
+		py = 610;
+	}
+
+	public void update(double dt) {
+		if (py > 80)
+			py -= 180 * dt;
+	}
+
+	public void draw(Graphics2D g2d) {
+		if (ativo) {
+			g2d.setPaint(new GradientPaint((float) px, (float) py, Color.black, (float) px, (float) py + 400,
+					Color.lightGray));
+			g2d.fill(new RoundRectangle2D.Double(px, py, 350, 420, 50, 50));
+			g2d.setColor(Color.black);
+			g2d.draw(new RoundRectangle2D.Double(px, py, 350, 420, 50, 50));
+			g2d.setColor(new Color(254, 233, 0));
+			g2d.fill(new Rectangle2D.Double(px + 120, py + 400, 110, 40));
+			g2d.setColor(new Color(254, 233, 0, 250));
+			g2d.setFont(new Font("Tahoma", Font.BOLD, 36));
+			g2d.drawString("HIGH SCORES", (int) px + 48, (int) py + 70);
+			g2d.setFont(new Font("Tahoma", Font.BOLD, 24));
+			for (int i = 0, h = (int) py + 110; i < 10; i++, h += 30) {
+				g2d.drawString(nomes[i] + " ", (int) px + 80, h);
+				g2d.drawString(" " + pts[i], (int) px + 230, h);
+			}
+			g2d.setColor(Color.black);
+			g2d.draw(new Rectangle2D.Double(px + 120, py + 400, 110, 40));
+			g2d.drawString("CLOSE", (int) px + 136, (int) py + 430);
+		}
+	}
+}
